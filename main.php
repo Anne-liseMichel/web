a4a8a5f0a7d80d50763bff018125ecf128803f71
Victor Sallard <sallardvictor@gmail.com>
2016-01-18 19:26:45 +0100

    Over for today;

--- main.php
+++ main.php
@@ -2,8 +2,8 @@
 	
 ?>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
@@ -15,7 +15,7 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li><a href="#ADD"> <?php echo($_SESSION['APPVAR']['ADD']) ?> </a></li>
+            <li><a href="#ADD"> ADD <?php echo($_SESSION['APPVAR']['ADD']) ?> </a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -24,8 +24,8 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        <h1>ZZTasks : The legacy version</h1>
+        <p class="lead">This is the test version, please be nice</p>
       </div>
 
     </div><!-- /.container -->
