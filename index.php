9c1ef80f06c37ca990dfcd386911cce4181e2ba0
Victor Sallard <sallardvictor@gmail.com>
2016-01-18 18:42:34 +0100

    Added things; Wondering if we'll manage to do it on time;

--- index.php
+++ index.php
@@ -2,14 +2,28 @@
 
 	if(!isset($_SESSION)) { 
 		session_start();
-		$french=file_get_contents("./data/french.json", "r");
-		$_SESSION['APPVAR'] = json_decode($french);
-		$_SESSION['AUTH'] = 0;
 	}
 
-	if(isset($_SESSION['AUTH']) && $_SESSION['AUTH']=1) {
-		header("Location: ./main.php");
-		exit;
+	if(!isset($_SESSION['LANG'])) { 
+		$_SESSION['LANG']='french.json';
+	}
+
+	$_SESSION['APPVAR'] = json_decode(file_get_contents('./data/' . $_SESSION['LANG'] ));
+
+	if(!isset($_SESSION['AUTH'])) { 
+		$_SESSION['AUTH']=0;
+	}
+
+	if(isset($_SESSION['AUTH']) && $_SESSION['AUTH']==1) {
+
+		$_SESSION['PAGENAME']='AM-VS TaskManager';
+		$_SESSION['PAGE']='main.php';
+
+	} else {
+
+		$_SESSION['PAGENAME']='AM-VS Login';
+		$_SESSION['PAGE']='signin.php';
+
 	}
 	
 ?>
@@ -24,34 +38,19 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>AM-VS Login</title>
+    <title> <?php echo $_SESSION['PAGENAME'] ?> </title>
 
     <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
 	
   </head>
 
   <body>
 
-    <div class="container">
+	<?php include $_SESSION['PAGE'] ?>
 
-      <form class="form-signin">
-        <h2 class="form-signin-heading"> <?php echo($_SESSION['APPVAR']['SIGNIN']); ?></h2>
-        <label for="inputEmail" class="sr-only"> <?php echo($_SESSION['APPVAR']['EMAIL']); ?> </label>
-        <input type="email" id="inputEmail" class="form-control" required autofocus>
-        <label for="inputPassword" class="sr-only"> <?php echo($_SESSION['APPVAR']['PASSWORD']); ?> </label>
-        <input type="password" id="inputPassword" class="form-control" required>
-		
-        <button class="btn btn-lg btn-primary btn-block" type="submit"> <?php echo($_SESSION['APPVAR']['LOGIN']); ?> </button>
-      </form>
-
-    </div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-	
   </body>
 </html>
