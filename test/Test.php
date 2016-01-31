2e1ad65339e7353ed1dd7a9d4d7e8e3c849db70a
Victor Sallard <sallardvictor@gmail.com>
2016-01-31 22:10:41 +0100

    Another round of tests;

--- test/Test.php
+++ test/Test.php
@@ -12,5 +12,20 @@
 			userAuth("toto","tata");
 			$this->assertFalse($_SESSION['AUTH']==1);
 		}
+
+		public function authorizedLogin(){
+			userAuth("david","david");
+			$this->assertTrue($_SESSION['AUTH']==1);
+		}
+
+		public function addNdelUser(){
+			userAddRemove("toto","tata");
+			$SHADOW = json_decode(file_get_contents('./data/users.json'),true);
+			$newhash=hash("sha256","totoSalt&Peppertata");
+			$this->assertTrue(isset($SHADOW[$newhash]));
+			userAddRemove("toto");
+			$SHADOW = json_decode(file_get_contents('./data/users.json'),true);
+			$this->assertFalse(isset($SHADOW[$newhash]));
+		}
 	}
 ?>
