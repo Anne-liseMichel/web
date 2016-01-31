863dec5ec06e30465048b9da503cd78f4c1efa21
Victor Sallard <sallardvictor@gmail.com>
2016-01-31 18:12:00 +0100

    Revised README;

--- php/Test.php
+++ php/Test.php
@@ -1,8 +1,13 @@
 <?php
 	Class ZZTest extends PHPUnit_Framework_TestCase
 	{
+		// Makes sure the tests work
 		public function testTest(){
 			$this->assertTrue(true);
+		}
+
+		public function authorizedLogin(){
+
 		}
 	}
 ?>
